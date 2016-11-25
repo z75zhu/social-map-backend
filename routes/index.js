06b2f43f511fac433fbee4f0d5cb18b46b7148b7
z75zhu <zhuzihaozachary@hotmail.com>
2016-11-25 14:35:20 -0500

    test for cloud

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/send', function(req, res) {
-  res.send('hello world');
+  res.send('hello world zihao');
 });
 
 router.post('/send', function (req, res) {
