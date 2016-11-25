a6cc813c42f98c08e15efd2397d694721c790e10
Zachary <zhuzihaozachary@hotmail.com>
2016-11-24 20:59:50 -0500

    add socket.io

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-	//localhost:3000/users/
-	//do nothing
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+// 	//localhost:3000/users/
+// 	//do nothing
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+// module.exports = router;
