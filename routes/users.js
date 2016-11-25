db039debfb86c435e6db3f98a5757f3992b776ef
Zachary <zhuzihaozachary@hotmail.com>
2016-11-24 20:22:57 -0500

    update package.jason

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	//localhost:3000/users/
+	//do nothing
   res.send('respond with a resource');
 });
 
